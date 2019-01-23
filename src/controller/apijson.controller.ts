900cbc10be18eac75cda3e554ec6cbde1d6162cc
unknown <zhangchijian@macauitzh.com>
2019-01-23 14:40:02 +0800

    加入视图渲染 Add View Rendering

--- src/controller/apijson.controller.ts
+++ src/controller/apijson.controller.ts
@@ -1,12 +1,18 @@
-import {Controller, Param, Body, Get, Post, Put, Delete} from "routing-controllers";
+import {Controller, Param, Body, Get, Post, Put, Delete, Render} from "routing-controllers";
 import { getRepository } from "typeorm";
 import { User } from "../entity/User";
 import { ApiJson } from "../apijson/apijson";
+import { async } from "rxjs/internal/scheduler/async";
 
 @Controller()
 export class ApiJsonController {
     @Post('/apijson')
     async apijson(@Body() body){
         return await ApiJson(body);
+    }
+    @Get('/apijson')
+    @Render('index.html')
+    async apijsonHtml(){
+        return {}
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,38 @@
 import "reflect-metadata"; // this shim is required
 require('dotenv').config()
 import { createConnection, getRepository } from "typeorm";
-import { createKoaServer } from "routing-controllers";
+import { createKoaServer, useKoaServer } from "routing-controllers";
 import { UserController } from "./controller/UserController";
 import { ApiJsonController } from "./controller/apijson.controller";
-import {useContainer as routingUseContainer} from "routing-controllers";
-import {useContainer as ormUseContainer} from "typeorm";
-import {Container} from "typedi";
+import { useContainer as routingUseContainer } from "routing-controllers";
+import { useContainer as ormUseContainer } from "typeorm";
+import { Container } from "typedi";
+import * as Koa from 'koa';
+import * as koaViews from 'koa-views';
+import * as path from 'path';
 
 routingUseContainer(Container);
 ormUseContainer(Container);
 
-// creates express app, registers all controller routes and returns you express app instance
-const app = createKoaServer({
+const koa = new Koa();
+koa.use(koaViews(path.join(__dirname, 'views'), {
+    options: {
+        ext: 'ejs',
+    },
+    extension: 'ejs',
+}))
+
+// use koa app, registers all controller routes and returns you koa app instance
+const app = useKoaServer(koa, {
+    // controllers: [`${__dirname}/controllers/**/*{.js,.ts}`],
     controllers: [UserController, ApiJsonController] // we specify controllers we want to use
-});
+})
+// creates koa app, registers all controller routes and returns you koa app instance
+// const app = createKoaServer({
+//     controllers: [UserController, ApiJsonController] // we specify controllers we want to use
+// });
  
-// run express application on port 3000
+// run koa application on port 3000
 // app.listen(3000);
 const databaseInitializer = async () => createConnection().then(async connection => {
     console.log('连接成功')
